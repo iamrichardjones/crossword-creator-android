fa0e4f89b1eee63c20e80601438c725279df80fc
Richard Jones <iamrichardjones@gmail.com>
2016-04-20 15:36:40 +0100

    add basic grid view and colour the background

--- app/src/main/java/info/richardjones/crossword/app/EntryPoint.java
+++ app/src/main/java/info/richardjones/crossword/app/EntryPoint.java
@@ -4,25 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
 import com.google.gson.Gson;
 
-import static com.google.common.collect.Lists.newArrayList;
-
 public class EntryPoint extends AppCompatActivity {
+
+    public static final Integer[] GRID_COLOURS = {
+            0xFFFFFFFF, 0x00FFFFFF, 0xFFFFFFFF, 0xFFFFFFFF,
+            0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFF000000,
+            0xFFFFFFFF, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF,
+            0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_point);
 
-        TextView tv = (TextView) findViewById(R.id.tv);
+//        TextView tv = (TextView) findViewById(R.id.tv);
 
         //test guava works
-        tv.setText(newArrayList("1", "2", "3").toString());
+//        tv.setText(newArrayList("1", "2", "3").toString());
 
         //test gson works
         Gson gson = new Gson();
+
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        MyAdapter adapter = new MyAdapter(EntryPoint.this);
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(EntryPoint.this, "D" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
---
+++ app/src/main/java/info/richardjones/crossword/app/MyAdapter.java
@@ -0,0 +1,56 @@
+package info.richardjones.crossword.app;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+public class MyAdapter extends BaseAdapter {
+
+    private Context context;
+
+    public MyAdapter(Context context) {
+        this.context = context;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+            gridView = inflater.inflate(R.layout.mobile, null);
+
+            // set value into textview
+            TextView textView = (TextView) gridView.findViewById(R.id.grid_item_label);
+            textView.setText("" + position);
+        } else {
+            gridView = convertView;
+        }
+
+        Integer gridColour = EntryPoint.GRID_COLOURS[position];
+        Log.d("ABC", "" + gridColour);
+        gridView.setBackgroundColor(gridColour);
+
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return EntryPoint.GRID_COLOURS.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+}
